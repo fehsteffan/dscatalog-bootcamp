8d2d6948a4c4c9e39b1ce8f3eba4f3070142b127
Felipe Steffan <felip.developer@hotmail.com>
2022-11-25 23:01:27 -0300

    Seeding Database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -15,7 +15,7 @@ public class Category implements Serializable {
 	
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -15,9 +16,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	
-	
-public List<Category> findAll(){
-	return repository.findAll();
+    @Transactional(readOnly = true)	
+    public List<Category> findAll(){
+	    return repository.findAll();
 	
    }	
 
