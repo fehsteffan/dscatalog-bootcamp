a34030fc6e7cc4e29e9f5c28cb8722e1f734e35a
Felipe Steffan <felip.developer@hotmail.com>
2022-11-27 21:31:59 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -19,11 +23,16 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
 	public Category() {		
 	}
 
-	public Category(Long id, String name) {
-		super();
+	public Category(Long id, String name) {		
 		this.id = id;
 		this.name = name;
 	}
@@ -42,8 +51,27 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}	
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+	
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
