7afab8e23cec5875174f7a8dab9f3cd5612654ff
Felipe Steffan <felip.developer@hotmail.com>
2022-11-29 22:04:50 -0300

    Product insert, update, delete

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -41,8 +41,7 @@ public class Product implements Serializable {
 	public Product() {		
 	}
 
-	public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
-		super();
+	public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {		
 		this.id = id;
 		this.name = name;
 		this.description = description;
