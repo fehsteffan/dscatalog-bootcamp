7fe3584410c728fc1f6d03a48ba0f7e93c8469cb
Felipe Steffan <felip.developer@hotmail.com>
2023-01-12 19:20:08 -0300

    User Service

--- backend/src/main/java/com/devsuperior/dscatalog/dto/RoleDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/RoleDTO.java
@@ -1,31 +1,30 @@
 package com.devsuperior.dscatalog.dto;
 
+import java.io.Serializable;
+
 import com.devsuperior.dscatalog.entities.Role;
 
-public class RoleDTO {
+public class RoleDTO implements Serializable {	
+	private static final long serialVersionUID = 1L;	
 	
 	private Long id;
 	private String authority;
 	
 	public RoleDTO() {
 		
-	}
-	
+	}	
 
 	public RoleDTO(Long id, String authority) {		
 		this.id = id;
 		this.authority = authority;
-	}
+	}	
 
 	public RoleDTO(Role entity) {		
 		id = entity.getId();
 		authority = entity.getAuthority();
-	}
-
-
-
-
-
+	}	
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -40,31 +39,5 @@ public class RoleDTO {
 
 	public void setAuthority(String authority) {
 		this.authority = authority;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		RoleDTO other = (RoleDTO) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}	
-
 }
--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dscatalog.dto;
 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
 import com.devsuperior.dscatalog.entities.User;
 
-public class UserDTO {
+public class UserDTO implements Serializable {	
+	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private String firstName;
@@ -10,27 +15,27 @@ public class UserDTO {
 	private String email;
 	
 	
-	public UserDTO() {		
-	}
+	Set<RoleDTO> roles = new HashSet<>();
 	
-
+	public UserDTO() {		
+	}	
+	
 	public UserDTO(Long id, String firstName, String lastName, String email) {		
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
+		
 	}
 	
 	public UserDTO(User entity) {		
 		id = entity.getId();
 		firstName = entity.getFirstName();
-		lastName = entity.getFirstName();
+		lastName = entity.getLastName();
 		email = entity.getEmail();
-	}
-	
-	
-	
-
+		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+		
+	}	
 
 
 	public Long getId() {
@@ -73,30 +78,8 @@ public class UserDTO {
 	}
 
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		UserDTO other = (UserDTO) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+	public Set<RoleDTO> getRoles() {
+		return roles;
 	}	
 
 }
